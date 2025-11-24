b96f9d7aaa5f4009ce8364dba44dc0c8f0d682eb
Toqeer Yousif <t.yousaf@nenergy.nl>
2025-11-24 23:08:39 +0500

    Added header script + production build

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      {/* blog page  */}
+      <iframe
+        src="https://offer.neueu.co/blog-8155"
+        className="w-full h-[100vh] border-0"
+        title="NeuEu Blog"
+      ></iframe>
+    </div>
+  );
+}
